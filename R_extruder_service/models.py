864d314d83046f713a6fa27dc7c5c9d82c7b43f1
Ante Krtalic <ante.krtalic@og-cs.com>
2023-11-27 08:01:58 +0100

    Create model for r script

--- R_extruder_service/models.py
+++ R_extruder_service/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+class RScript(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    fileName = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+
+    class Meta:
+        db_table = "scripts"
+        ordering = ["-createdAt"]
+        def __str__(self) -> str:
+            return self.fileName
