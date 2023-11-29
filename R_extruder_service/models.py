9ebff6d010213e8358ffb61d58d4b8bbe8cbaf66
Mihael Maric <mihael.maric@og-cs.com>
2023-11-29 14:10:50 +0100

    DEV-331: Added WordDocument Model and Made migrations

--- R_extruder_service/models.py
+++ R_extruder_service/models.py
@@ -8,3 +8,12 @@ class RScript(models.Model):
     @property
     def script_path(self):
         return os.path.basename(self.script.name)
+
+class WordDocument(models.Model):
+    document = models.FileField(upload_to='word_documents/')
+    file_name = models.CharField(max_length=255)
+    file_content = models.BinaryField()
+
+    @property
+    def document_name(self):
+        return os.path.basename(self.document.name)
--- R_extruder_service/serializers.py
+++ R_extruder_service/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from R_extruder_service.models import RScript
+from R_extruder_service.models import RScript, WordDocument
+
 
 class RScriptSerializer(serializers.ModelSerializer):
     class Meta:
         model = RScript
         fields = ('script',)
+
+class WordDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WordDocument
+        fields = ('document', 'file_name', 'file_content',)
