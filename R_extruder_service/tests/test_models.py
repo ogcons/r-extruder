9b17696caedba8f21ee4fc1119238e0007dc94a6
Mihael Maric <mihael.maric@og-cs.com>
2023-12-05 11:54:49 +0100

    DEV-307: Changes made based on code review.

--- R_extruder_service/tests/test_models.py
+++ R_extruder_service/tests/test_models.py
@@ -1,25 +1,44 @@
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.exceptions import ObjectDoesNotExist
 from ..models import RScript, WordDocument
 import os
 
 class RScriptModelTest(TestCase):
-    def setUp(self):
-        self.sample_script = RScript.objects.create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # create RScript instance
+        cls.sample_script = RScript.objects.create(
             script=SimpleUploadedFile("test_script.R", b"file_content")
         )
-        self.rscript = RScript.objects.create(script='test_file')
+        cls.rscript = RScript.objects.create(script='test_file')
 
-        self.sample_document = WordDocument.objects.create(
+        # create WordDocument instance
+        cls.sample_document = WordDocument.objects.create(
             document=SimpleUploadedFile("test_document.docx", b"document_content"),
             file_name='test_document',
             file_content=b"document_content"
         )
-        self.word_document = WordDocument.objects.create(document='test_file.docx', file_name='test_file', file_content=b"file_content")
+        cls.word_document = WordDocument.objects.create(document='test_file.docx', file_name='test_file',
+                                                        file_content=b"file_content")
+    @classmethod
+    def tearDownClass(cls):
+        # delete testFiles after tests
+        try:
+            os.remove(cls.sample_script.script.path)
+        except (ObjectDoesNotExist, FileNotFoundError):
+            pass
 
+        try:
+            os.remove(cls.sample_document.document.path)
+        except (ObjectDoesNotExist, FileNotFoundError):
+            pass
+
+        super().tearDownClass()
 
     def test_rscript_creation(self):
-
         # when
         rscript = self.rscript
 
@@ -27,7 +46,6 @@ class RScriptModelTest(TestCase):
         self.assertEqual(rscript.script, 'test_file')
 
     def test_script_path_property(self):
-
         # when
         expected_path = os.path.basename(self.sample_script.script.name)
 
@@ -39,16 +57,22 @@ class RScriptModelTest(TestCase):
         self.assertEqual(actual_content, "file_content")
 
     def test_word_document_creation(self):
+        # when
         word_document = self.word_document
+
+        # then
         self.assertEqual(word_document.document, 'test_file.docx')
         self.assertEqual(word_document.file_name, 'test_file')
         self.assertEqual(word_document.file_content, b"file_content")
 
     def test_document_name_property(self):
+        # given
         expected_name = os.path.basename(self.sample_document.document.name)
 
+        # when
         with self.sample_document.document.open('rb') as document_file:
             actual_content = document_file.read()
 
+        # then
         self.assertEqual(self.sample_document.document_name, expected_name)
         self.assertEqual(actual_content, b"document_content")
--- R_extruder_service/tests/test_views.py
+++ R_extruder_service/tests/test_views.py
@@ -6,10 +6,8 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.conf import settings
-
 from ..models import RScript
 from ..serializers import RScriptSerializer
-
 
 class PostRunRScriptViewTest(TestCase):
     def setUp(self):
@@ -19,47 +17,18 @@ class PostRunRScriptViewTest(TestCase):
     def test_post_run_r_script_view(self):
         # Temporary R Script
         r_code = """
-                M027 = list(
-       time = c(    0,     0,  0.25,  0.25,     1,     1,     2,     2,     7,     7,    15,    15,    21,    21,    30,    30,    60,    60,    91,    91,   120,   120,   123,   123),
-    residue = c( 98.9, 101.5,  97.7,  99.5,  99.2,  98.0,  97.0,  98.4,  94.0,  91.7,  79.3,  79.3,  84.6,  78.0,  68.8,  70.5,  47.2,  48.5,  38.4,  38.3,  31.7,  30.8,  29.7,  28.8),
-     weight = c(    1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1),
-                      to = c("M054","M047"),
-         FF = list(ini   = c(0.33,0.33),
-                   fixed = c(0,0),
-                   lower = c(0.0,0.0),
-                   upper = c(1.0,1.0)),
-                   sink  = TRUE,
-       type = "SFO",
-          k = list(ini   = 0.01055,
-                   fixed = 0,
-                   lower = 0.0,
-                   upper = Inf),
-         M0 = list(ini   = 98.69,
-                   fixed = 0,
-                   lower = 0.0,
-                   upper = Inf))
-    
+                # Create some sample data
+                x <- 1:10
+                y <- c(2, 4, 6, 8, 10, 8, 6, 4, 2, 0)
 
-
-       
-       # Plotting M027
-       plot(M027$time, M027$residue, type = "b", pch = 16, col = "blue",
-            xlab = "Time", ylab = "Residue", main = "Time vs Residue (M027)")
-       
-       # Adding labels
-       text(M027$time, M027$residue, labels = 1:length(M027$time), pos = 3, col = "red")
-        """
+                # Create a basic plot
+                plot(x, y, type = "l", col = "blue", lwd = 2, main = "Simple Plot", xlab = "X-axis", ylab = "Y-axis")
+                """
         script_file = SimpleUploadedFile("script.R", r_code.encode("utf-8"), content_type="text/plain")
         r_script_data = {'script': script_file}
 
-        # Print data
-        print("Test data:", r_script_data)
-
         # Simulate POST
         response = self.client.post('/api/extractors/', {'script': script_file}, format='multipart')
-
-        # Print response
-        print("Response content:", response.content)
 
         # Asserts
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -97,13 +66,13 @@ class RScriptRetrieveViewTest(TestCase):
         self.client = APIClient()
 
     def test_retrieve_rscript(self):
-        # Given
-        r_script = RScript.objects.create(script="Script content")  # Adjust this according to your model structure
+        # given
+        r_script = RScript.objects.create(script="Script content")
 
-        # When
+        # when
         response = self.client.get(reverse('rscript-retrieve', kwargs={'pk': r_script.pk}))
 
-        # Then
+        # then
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         serialized_data = RScriptSerializer(r_script).data
         self.assertEqual(response.data, serialized_data)
